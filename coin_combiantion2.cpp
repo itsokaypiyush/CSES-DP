ac9d8ad8dd860c74c0b81e803bbfd8798d359302
Piyush Mishra <piyushmishra464673@gmail.com>
2026-09-24 23:27:49 +0530

    Add files via upload

---
+++ coin_combiantion2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> coins(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    const int MOD = 1e9 + 7;
+
+    vector<int> dp(x + 1, 0);
+    dp[0] = 1;
+
+    for (int coin : coins) {
+        for (int sum = coin; sum <= x; sum++) {
+            dp[sum] += dp[sum - coin];
+            dp[sum] %= MOD;
+        }
+    }
+
+    cout << dp[x] << endl;
+
+    return 0;
+}
